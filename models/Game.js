34cf5839a4618417982f394e13a2424f46504fa6
genkidesu <tysaunders1@hotmail.com>
2021-05-31 21:43:33 +1000

    added models

---
+++ models/Game.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Game extends Model { }
+
+// game table
+
+Game.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        game_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        review_ids: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: 'reviews',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'game',
+    }
+);
+
+module.exports = game;
---
+++ models/Review.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model { }
+
+// review store
+
+Review.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        review_text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        game_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'game',
+                key: 'id'
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'game',
+    }
+);
+
+module.exports = game;
