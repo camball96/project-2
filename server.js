dec2a1fd48a7ee3b631bcb43dce6f8ad775c5e04
Frostwrath <nbainbridge@outlook.com.au>
2021-06-01 19:29:16 +1000

    added basic schema, express.js and connection.js files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const app = express();
+const sequelize = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Now listening on PORT:${PORT}!`));
+});
