37a218c1abb63a64cb52b99a056ae8ae6429b271
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-06-03 18:39:10 +0800

    added styling for game profile and game review sections, updating handlebars for each is WIP

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,2 @@
 // this is static js for webpage
 
-// let i = 0;
-// let txt = "GAMER'S HIDEOUT"; /* The text */
-// let speed = 50; /* The speed/duration of the effect in milliseconds */
-
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.querySelector(".headerTypewriter").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
