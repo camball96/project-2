f2005af89f0fc2fe8311878ae68536a05d897193
genkidesu <tysaunders1@hotmail.com>
2021-06-07 22:33:42 +1000

    various things

--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,35 @@
+
 async function login(e) {
     e.preventDefault();
-
+    console.log(e.target.className)
     const username = document.querySelector('#username').value;
     const password = document.querySelector('#password').value;
-    var errorMsg = document.querySelector('#problem');
 
-    if (password.length > 5) {
-        if (username) {
+    if (username && password) {
 
-            // goes to /api /user / login route to check user/pass against DB
-            const verification = await fetch('/api/user/login', {
-                method: 'post',
-                credentials: 'same-origin',
-                body: JSON.stringify({
-                    user_name: username,
-                    password: password
-                }),
-                headers: { 'Content-Type': 'application/json' }
+        // goes to /api /user / login route to check user/pass against DB
+        const verification = await fetch('/api/user/login', {
+            method: 'post',
+            credentials: 'same-origin',
+            body: JSON.stringify({
+                user_name: username,
+                password: password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        })
+
+        // converts the response to useable data then sends them to home if log in success other wise it displays an error.
+        console.log(verification)
+        verification.text()
+            .then((response) => {
+                response == 1
+                    ? window.location.replace('/')
+                    : errorMsg.innerText = response
             })
-
-            // converts the response to useable data then sends them to home if log in success other wise it displays an error.
-            console.log(verification)
-            verification.text()
-                .then((response) => {
-                    console.log(response)
-                    response == 1
-                        ? window.location.replace('/')
-                        : errorMsg.innerText = response
-                })
-            return
-        }
-        errorMsg.innerText = 'Username must be filled out'
         return
     }
-    errorMsg.innerText = 'Password needs to be 6 characters minimum'
+    // user only reaches this code if they left something blank
+    document.querySelector('#problem').innerText = 'Both username and password must be filled out'
 }
 
 
