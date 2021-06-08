4db99ff714cd266ac0d9bbbd51e4cab55556faad
Cam <cam.ball96@hotmail.com>
2021-06-08 16:31:33 +1000

    updated primary and foreign keys in seeds

--- db/seeds/user-seeds.js
+++ db/seeds/user-seeds.js
@@ -2,21 +2,25 @@ const { User } = require('../../models');
 
 const userData = [
     {
+        user_id: 1
         user_name: "Nick B",
         user_email: "email@email.com",
         password: "passwerd"
     },
     {
+        user_id: 2
         user_name: "Ty S",
         user_email: "email1@email.com",
         password: "passwerd"
     },
     {
+        user_id: 3
         user_name: "Aaron V",
         user_email: "email2@email.com",
         password: "passwerd"
     },
     {
+        user_id: 4
         user_name: "Cam B",
         user_email: "email3@email.com",
         password: "passwerd"
