5a67bfc6a501a35343f748b6ddb2ab2895f1dbc4
genkidesu <tysaunders1@hotmail.com>
2021-06-09 13:40:12 +1000

    search

---
+++ public/js/searchResults.js
@@ -0,0 +1,21 @@
+
+// search for a game by name
+
+async function searchGame() {
+
+    criteria = document.querySelector("#searchBar").value
+
+    const searchResult = await fetch(`/search/${criteria}`, {
+        method: 'get',
+        credentials: 'same-origin',
+        headers: { 'Content-Type': 'application/json' }
+    })
+        .then((searchResult) => {
+            searchResult.redirected
+                ? window.location.replace(searchResult.url)
+                : console.log('no results were found')
+        })
+}
+// Listeners.
+
+document.querySelector('.searchButton').addEventListener('click', searchGame);
