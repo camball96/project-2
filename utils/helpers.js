db10b0c3cd1ad898a19249c9f96b4ad0ff56f6c1
genkidesu <tysaunders1@hotmail.com>
2021-06-13 03:39:13 +1000

    changed syntax on averageOut and script srcs for heroku issue

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,7 @@
 // helper functions for iterating over returned data
 module.exports = {
 	averageOut: (reviews) => {
-		var total = reviews.reduce((acc, review) => acc + review.review_score, 0);
+		var total = reviews.reduce((acc, review) => { acc + review.review_score }, 0);
 		var average = total / reviews.length;
 		return average.toFixed(1);
 	},
