78ed37d17234d1b074e781a2bcb6237ce9240d0c
genkidesu <tysaunders1@hotmail.com>
2021-06-13 12:39:42 +1000

    removed review user id from query

--- routes/api/review-routes.js
+++ routes/api/review-routes.js
@@ -4,7 +4,6 @@ const { User, Game, Review } = require("../../models");
 
 router.post('/add', async (req, res) => {
 	req.body.user_name = req.session.user_name
-	req.body.user_id = req.session.user_id
 	try {
 		const addReview = await Review.create(req.body)
 		return res.status(200).json(addReview);
